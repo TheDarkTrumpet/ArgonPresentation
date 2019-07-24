2a7d1136de6b18873a1c974637d7a22364bc2682
David Thole <david@thedarktrumpet.com>
2019-07-24 11:04:26 -0500

    Copied RestartableJobsSRP to Checkpoint, as a starting point

---
+++ RestartableJobsCheckpoint/runme.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SCRIPTDATE=`date +"%y-%m-%d"`
+TEMPLATELOCATION="\/nfsscratch\/restartable-$SCRIPTDATE"
+
+sed -i "s/<TEMPLATELOCATION>/$TEMPLATELOCATION/" settings.cfg
+
+QUEUETIME=`date +"%D %T"`
+echo "Starting Queue (Job1): $QUEUETIME"
+job1_jid=$(qsub -terse scripts/job1.sh)
+
+echo "Starting Queue (Job2)"
+job2_jid=$(qsub -hold_jid $job1_jid -terse scripts/job2.sh)
+
+echo "Job IDs: Job1 - $job1_jid, Job2 - $job2_jid"
---
+++ RestartableJobsCheckpoint/scripts/job1.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#$ -q all.q
+#$ -N RestartableJobsSRP-job1
+#$ -pe smp 30
+#$ -j y
+#$ -cwd
+#$ -ckpt user
+
+source settings.cfg
+
+if [ ! -d "$TEMPLOCATION" ]; then
+    mkdir $TEMPLOCATION
+fi
+
+if [ $RESTARTED = 1]; then
+    cat "Restarted..." >> $TEMPLOCATION/job1.txt
+fi
+
+sleep 3600
+
+cat `uname -a` >> $TEMPLOCATION/job1.txt
